67e13804d11a6461352987d8bd81c9e3d552d1cf
felipeMurilloUrbina <40193021+felipeMurilloUrbina@users.noreply.github.com>
2018-07-13 00:03:27 -0600

    cambios 13.07
    Se agrega component de login, se agrega background avika, se agregan metodos de activar y desactivar cargando.

--- src/app/_servicios/base.service.ts
+++ src/app/_servicios/base.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { ToasterService } from 'angular2-toaster';
+declare var $: any;
 
 
 @Injectable({ providedIn: 'root' })
 export class BaseService {
 
     private actionUrl: string;
-
-    constructor(public http: HttpClient, public endPoint: string) {
+    private toasterService: ToasterService;
+    constructor(public http: HttpClient, public endPoint: string, toasterService: ToasterService) {
         this.actionUrl = environment.urlBase + endPoint;
     }
 
@@ -38,4 +40,31 @@ export class BaseService {
     public delete<T>(id: number) {
         return this.http.delete<T>(this.actionUrl + id);
     }
+
+    public enviarMensaje(opcion, titulo, mensaje) {
+        this.toasterService.pop(opcion, titulo, mensaje);
+    }
+
+    activarEsperando() {
+        $(document).ready(() => {
+          $('body').waitMe({
+            effect: 'bounce',
+            text: 'Cargando...',
+            bg: 'rgba(255,255,255,0.6)',
+            color: '#007bff',
+            maxSize: '',
+            waitTime: -1,
+            source: '',
+            textPos: 'vertical',
+            fontSize: '18',
+            onClose: function() {}
+          });
+        });
+        }
+    cerrarEsperando() {
+        $(document).ready(() => {
+            $('body').waitMe('hide');
+          });
+    }
+    
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../_servicios/login.service';
+ declare var $: any;
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
+})
+export class LoginComponent implements OnInit {
+
+  loadingVisible = false;
+  loadIndicatorVisible = false;
+  tituloBoton = 'Entrar';
+  titulo: String = 'Iniciar Sesion';
+  sucursales = [];
+  sucursalId = '';
+  isBlocked =  false;
+  usuario: Usuario;
+  constructor(private router: Router, private _fb: FormBuilder, private _service: LoginService) {
+    localStorage.removeItem('token');
+}
+
+ngOnInit() {
+  this.usuario = new Usuario();
+  // $(document).ready(() => {
+  //   console.log('a');
+  //   setTimeout(() => {
+  //     this._service.activarEsperando();
+  //   }, 100);
+  // });
+  }
+
+  showMessage(option, title, message) {
+    switch (option) {
+      case 1:
+        this.msgs.push({severity: 'success', summary: title, detail: message});
+      break;
+      case 2:
+        this.msgs.push({severity: 'error', summary: title, detail: message});
+      break;
+      case 3:
+        this.msgs.push({severity: 'warn', summary: title, detail: message});
+      break;
+    }
+  }
+
+  cambiarEstadoBoton(texto: string, bandera: boolean) {
+    this.loadIndicatorVisible = bandera;
+    this.loadingVisible = bandera;
+  }
+
+  keydown(event) {
+    if (event === 13) {
+      this.login();
+    }
+   }
+
+  login() {
+    if (this.sucursales.length > 0) {
+      localStorage.setItem('sucursal', this.usuario.SucursalId.toString());
+      this.router.navigate(['/admin']);
+    }
+    // Valida si la contraseña esta vacia.
+    if ((!this.usuario.Contra) || (this.usuario.Contra === '')) {
+      $('#msg-contra').addClass('alert-validate');
+      return false;
+    }
+    this._service.activarEsperando();
+    this._service.postGenerico(this.usuario, '/usuarios/login').subscribe(data => {
+      this.sucursales = data.Sucursales;
+      localStorage.setItem('sucursales',  JSON.stringify(this.sucursales));
+      localStorage.setItem('token',  data.Token);
+      this.tituloBoton = 'Seleccione Empresa';
+      this._service.cerrarEsperando();
+    }, error => {
+      console.log(error);
+      if (!error._body.isTrusted) {
+        let er = eval('(' + error._body + ')');
+        if (er) {
+          this.showMessage(2, 'error', er.Message);
+        } else {
+          this.showMessage(2, 'Error', 'Error, al conectar con la base de datos.');
+        }
+      } else {
+        this.showMessage(2, 'Error', 'Error, al conectar con la base de datos.');
+      }
+      this._service.cerrarEsperando();
+    }, () => {
+    });
+  }
+
+}
